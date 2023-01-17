7a5c80645f653a708aef4911ffad9173ea8801b5
msawhitney <msawhit3@gmail.com>
2023-01-17 10:43:16 -0800

    completed assessment 5

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,6 +12,20 @@
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
+// Pseudocode
+// Input: string
+// Output: string with these letters converted as follows: 
+// a to 4
+// e to 3
+// i to 1
+// o to 0
+// Process: Create a function called secretCode
+// Iterate through the string and find each of the letters (a, e, i, and o) so that I can change them. Possibly I can use the string method string.charAt(i). 
+// Also, look at regex methods.
+//I ended up using a for..in loop which I had never heard of (see reference below) to iterate through the string and find the letter/number pair in the object called mappingCode.
+ //I also used replace and a regex method to search (reference also below). More explanation is below.
+
+
 // a) Create a test with expect statements using the variables provided.
 
 const secretCodeWord1 = "Lackadaisical"
@@ -21,9 +35,68 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe ('secretCode', (string, mapping) => {
+    it ('Create a function that takes in a string and returns a string with a coded message.', () => {
+  expect(secretCode(secretCodeWord1, mappingCode)).toEqual("L4ck4d41s1c4l") 
+  expect(secretCode(secretCodeWord2, mappingCode)).toEqual("G0bbl3dyg00k")
+  expect(secretCode(secretCodeWord3, mappingCode)).toEqual("3cc3ntr1c")    
+    })
+  })
+
+//   RED --GOOD FAIL 
+//   FAIL  ./code-challenges.test.js
+//   secretCode
+//     ReferenceError: secretCode is not defined
+
 // b) Create the function that makes the test pass.
 
+const secretCode = (string, mapping) => {
+  //My function secretCode takes in a string and an object(mapping).
+  //The object describes what to replace in the string.
+
+  let newString = string;
+  //newString is set to the value of the "string" argument
+
+  //The function uses a "for...in" loop. Here is a reference for those: https://www.w3schools.com/js/js_loop_forin.asp
+  //It also uses a regular expression (RegExp) with modifiers gi (global/case insensitive). Here is a reference for those: https://www.w3schools.com/jsref/jsref_obj_regexp.asp#:~:text=RegExp%20Object,pattern%20with%20Properties%20and%20Methods.
+
+  //The for...in loop will iterate through the mapping object (called mappingCode) and continue until all keys in the mapping object have been found and replaced.
+  //For key (letter) in object (mapping)
+  for (let letter in mapping) {        
+      let regularExp = new RegExp(letter, 'gi');
+      //Create a new regex using the current letter from the mapping object. 
+      //Modifier gi means global and case insensitive - so every occurance of the letter can be found and case won't matter. A regex is a sequence of characters that define a search pattern (here, the vowels that need to be replaced with numbers.)
+      
+      //Use the replace method on the newString variable, passing in the regex and the corresponding value from the mapping object.
+      newString = newString.replace(regularExp, mapping[letter]);
+      //On each iteration, the loop will look up a value in the object using the key. 
+      //It will assign the new value to the variable called letter.
+  }
+  //The loop continues until all keys in the mapping object have been found and replaced. Then the new string is returned.
+  return newString;
+  }
+  const mappingCode = {
+       "a": "4",
+       "e": "3",
+      "i": "1",
+       "o": "0",
+  }
+//----GREEN --- PASSED
+// secretCode
+//See below
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
+
+// Pseudocode
+// Input: (1) array of strings, (2) a single character (string)
+// Output: all the words from the array (1) that contain the character in (2)
+// Process: Create a function called findAletter
+// Probably want to use .filter and .includes. 
+
+// You would need to use .filter the fruitArray and select elements that match a condition. 
+//The condition is that the word must include the letter that is passed to the function. ---EDIT TO ADD: include upper/lowercase!
+//Use the .includes method to check for that. 
+// ---Jest TEST failed after function was created because of the capital letter A (Apricot). I refactored to account for upper and lowercase letters. ----TEST PASSED
 
 // a) Create a test with expects statement using the variable provided.
 
@@ -34,9 +107,50 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+describe ('filterWords', (array, letter) => {
+    it ('Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.', () => {
+  expect(filterWords (fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"]) 
+  expect(filterWords (fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"]) 
+    })
+  })
+
+//   RED Good FAIL
+//   FAIL  ./code-challenges.test.js
+//     ● filterWords › 
+//   ReferenceError: filterWords is not defined
+   
+
 // b) Create the function that makes the test pass.
 
+const filterWords = (array, letter) => {
+  //A new array called filteredWords is created (to store the filtered words.)
+  const filteredWords = array.filter(word => {
+      //Create variables to use to account for upper or lowercase letters.
+      let lowerCaseLetter = letter.toLowerCase();
+      let upperCaseLetter = letter.toUpperCase();
+      return word.includes(lowerCaseLetter) || word.includes(upperCaseLetter);
+     //Check if the word includes the desired letter in uppercase OR lowercase.
+   });
+   return filteredWords;
+   //Return the array of filtered words
+}
+//----GREEN --- PASSED
+// filterWords
+//See below
+
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
+
+// Pseudocode
+// Input: array
+// Output: true or false
+// Process: Full house = n1, n1, n1 and n2, n2
+// Create a function called fullHouse.
+// Sort the array to make it easier to find matches
+// Iterate through with a for loop.
+// If iteration finishes and there is a non-match, return false.
+// If iteration finds matches, keep count with variables n1 and n2.
+// ----When n1 equals 3 and n2 equals 2, return true. ---(changed this)
+// ----Jest TEST FAILED after function set-up because hand3 did not go as expected. I had to add the matchOftwos variable to make sure there was a match of 3 AND two (see below.) I changed it to the following: if n1 equals 3 and matchOftwos is true - return true.-----TEST PASSED
 
 // a) Create a test with expect statements using the variable provided.
 
@@ -49,4 +163,76 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+describe ('fullHouse', (array) => {
+    it ('Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.', () => {
+  expect(fullHouse(hand1)).toEqual(true) 
+  expect(fullHouse(hand2)).toEqual(false)
+  expect(fullHouse(hand3)).toEqual(false)
+  expect(fullHouse(hand4)).toEqual(true)   
+    })
+  })
+
+//   ------------RED Good FAIL------
+//   FAIL  ./code-challenges.test.js
+//   ● fullHouse › 
+//     ReferenceError: fullHouse is not defined
+
 // b) Create the function that makes the test pass.
+
+const fullHouse = (array) => {
+  array.sort();
+  let n1 = 0;
+  let n2 = 0;
+  let matchOftwos = false;
+  let previous;
+  //Sorted the array.
+  //Declared two variables to keep count of the duplicate numbers. 
+  //-----Added another variable to make sure I have a match of 2s as well as 3s. (failed Jest test called this out). Initialized it as false and created an if statement so --- if n2 equals 2, matchOftwos will be true.-----//
+  //About "previous" variable --- declared before it is defined to use it inside the function.
+  //This is so I can compare each number in the array to the previous number.
+  
+  for (let i=0; i<array.length; i++) {
+      //starting at index zero; going the length of the array; adding 1 to the index to iterate through
+  
+          if (array[i] !== previous) {
+              n1 = 1;
+              //check if the element in the array is not equal to the previous number. 
+              //If not, set n1 to 1 to start count.
+  
+            if (array[i] === array[i + 1])  {
+              n2++;
+              //check if the current element is equal to the next element. If it is, add 1 to the count.
+
+              if (n2 === 2) matchOftwos = true;
+              // Trigger change of the matchOftwos variable to true if n2 count equals 2. This is so both a 3 match AND a 2 match will be present to indicate a full house.
+
+              } else {
+              n2 = 1;  
+              //If current element is not equal to next, then set n2 to 1 to start the second group.
+              }
+              //Store the current element as "previous" for the next iteration.
+              previous = array[i];
+              
+          } else { 
+            //If the current element is equal to the previous element, increase n1 count by 1.
+            n1++;
+          }   
+        }    
+          if (n1 === 3 && matchOftwos) {
+            // if n1 equals 3 AND there is a matchOftwos (true), you have a full house!    
+            return true;
+          }
+        //If matches are not found, there is no full house so return false.
+        return false;
+      };
+
+//GREEN TEST ---- 3 passed tests.
+
+// PASS  ./code-challenges.test.js
+//   secretCode
+//     ✓ Create a function that takes in a string and returns a string with a coded message. (1 ms)
+//   filterWords
+//     ✓ Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. (1 ms)
+//   fullHouse
+//     ✓ Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
+
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,6 +3,17 @@
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
 
+# Pseudocode
+# Input: array, single letter
+# Output:  array of all the words containing that letter
+# Process: Create a function called find_words. 
+# Create an array called matching_words.
+# Iterate over each element/word in the original array and use include to see if it contains the required letter. 
+# If it does add it to the created array. 
+# Return the created array.
+# Actual output as expected: ["coffee", "soda water"]
+# ["tea", "water", "soda water"]
+
 beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
@@ -10,19 +21,67 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def find_words(array, letter)
+    matching_words = []
+    #Iterate over each element in array - specify a variable called |word| 
+    array.each do |word|
+        #Check if word includes the required letter 
+        if word.include?(letter)
+        #If it does, add it to the matching_words array
+        matching_words << word   
+        end
+    end   
+    #Return the array of matching words
+    return matching_words
+end         
+
+p find_words(beverages_array, letter_o)
+p find_words(beverages_array, letter_t)
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
+# Pseudocode
+# Input: hash
+# Output: a non-nested array that has all hash values 
+# 1. at their own index 
+# 2. alphabetical order
+# Process: I'm reading about flattening arrays. 
+# Create a variable called alpha_states to hold the result of flatten method (https://www.geeksforgeeks.org/ruby-hash-flatten-function/) 
+# Chain the following: us_states, .values (to access values of us_states hash), .flatten, and then .sort (to alphabetize)
+# Actual output as expected: ["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Rhode Island", "Washington"]
+
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+alpha_states = us_states.values.flatten.sort
+# Variable (alpha_states) holds result of flattening values of hash (us_states) and those values are sorted (.sort)
+
+p alpha_states
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+# Pseudocode
+# Create a Bike class and initialize it with default values. 
+# Create a method that uses string interpolation to return a sentence about bike data. 
+# Create an instance of the bike and print data. (Moved to define outside class to prevent error - see below)
+# Actual output as expected: "The Trek bike has 2 wheels and is going 0 mph."
 
+class Bike
+    def initialize (model, wheels=2, current_speed=0)
+        # Initialize a new Bike object with a model, wheels, and current speed.
+        # Default number of wheels is 2, default speed is 0
+        @model = model
+        @wheels = wheels
+        @current_speed = current_speed
+    end
+    def bike_info
+        # Method returns a sentence with all the bike data.
+        p "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end    
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -30,3 +89,44 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+# Pseudocode
+# Input: number 
+# Output: for pedal_faster method - current_speed increased by given number
+#         for brake method - current_speed reduced by given number unless current_speed is already less than given number (then current_speed is reduced to zero.) 
+# Process: 
+# 1. create a method called pedal_faster that adds given number to current speed
+# 2. create a method called brake that uses a conditional statement to assign value to current_speed. Conditional statement checks to see if current_speed minus given number is greater than zero. If that is true, decreases current_speed by given number.
+# If that is false, sets current_speed to zero.          
+# Actual output as expected: 
+# 10
+# 28
+# 23
+# 0
+
+def pedal_faster (speed)
+    #Increases the speed by the given amount
+    @current_speed += speed
+end
+
+def brake(speed)
+    #If the current speed is more than the given amount, current speed is reduced by the given amount.
+    if (@current_speed - speed) > 0
+        @current_speed -= speed
+    #Otherwise, the current speed is reduced to zero.    
+    else
+        @current_speed = 0
+    end
+end
+end           
+
+#Creates an instance of the bike, Trek model.
+my_bike = Bike.new("Trek")
+#Calls the method bike_info (prints sentence about bike info.)
+my_bike.bike_info
+
+#calls pedal_faster and brake methods on my_bike (with speed given as argument), prints current speed. Output as expected (see above)
+p my_bike.pedal_faster(10)
+p my_bike.pedal_faster(18)
+p my_bike.brake(5)
+p my_bike.brake(25)
